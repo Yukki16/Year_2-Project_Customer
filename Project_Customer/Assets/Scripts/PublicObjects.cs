5aa5f2e2449528271e53d61136f430c61e3dfbe4
Boldea Marius <yukki.github@gmail.com>
2022-09-06 14:58:26 +0200

    Small Changes

---
+++ Project_Customer/Assets/Scripts/PublicObjects.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicObjects : MonoBehaviour
+{
+    public GameObject[] ground;
+    public GameObject[] trash;
+}
