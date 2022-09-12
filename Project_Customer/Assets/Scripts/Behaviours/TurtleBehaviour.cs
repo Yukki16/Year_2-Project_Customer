045935f472059b53b683f305e7bec728bde13fe2
Boldea Marius <yukki.github@gmail.com>
2022-09-12 04:00:07 +0200

    Tried to add the seagull

--- Project_Customer/Assets/Scripts/Behaviours/TurtleBehaviour.cs
+++ Project_Customer/Assets/Scripts/Behaviours/TurtleBehaviour.cs
@@ -1,28 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TurtleBehaviour : MonoBehaviour
 {
+    //The terrain area
     private Terrain playArea;
 
     private float offset;
-
+    //script to help with the AI in NavMesh
     Mover mover;
 
     Vector3 targetObjPosition;
-
+    //GameObject for when the turtle's target spawns, it becomes it's parent;
     private GameObject turtleSpawnerParent;
-
+    //The empty target who is a child of turtleSpawnerParent;
+    //it helps with organization
     GameObject targetObj;
+
+    private GameObject seagulSpawner;
 
     void Start()
     {
+        //creates the empty object as a target
         targetObj = new GameObject();
+        
         turtleSpawnerParent = GameObject.FindGameObjectWithTag("TurtleTargets");
+        seagulSpawner = GameObject.FindGameObjectWithTag("SeagulSpawner");
         mover = GetComponent<Mover>();
+
         playArea = Terrain.activeTerrain;
+
         StartCoroutine(WiggleTarget());
+
         SpawnTarget();
     }
 
@@ -30,23 +41,33 @@ public class TurtleBehaviour : MonoBehaviour
     {
         targetObj.transform.position = position;
     }
-
+    /// <summary>
+    /// Moves the target under a new parent for organization. The target is finally set for the turtle.
+    /// </summary>
     void SpawnTarget()
     {
         targetObj.transform.SetParent(turtleSpawnerParent.transform);
+        //Moves target to the end of the terrain(Z axis) where is the water, where x and y are the turtle position
         targetObjPosition = new Vector3(transform.position.x, transform.position.y, playArea.terrainData.size.z);
         targetObj.transform.position = targetObjPosition;
         mover.SetTarget(targetObj.transform);
     }
-
+    /// <summary>
+    /// Destroys turtle in contact with trash
+    /// </summary>
+    /// <param name="collision"></param>
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "draggable")
         {
-            Destroy(gameObject);
+            this.gameObject.GetComponent<NavMeshAgent>().acceleration /= 2;
+            seagulSpawner.GetComponent<SpawnSeaguls>().SpawnSeagul(this.gameObject);
+            //Destroy(gameObject);
         }
     }
-
+    /// <summary>
+    /// Arrived on destination, both the target and the turtle are destroyed
+    /// </summary>
     void DestroyOnTarget()
     {
         if (Vector3.Distance(transform.position, targetObj.transform.position) < 5)
@@ -55,7 +76,10 @@ public class TurtleBehaviour : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
+    /// <summary>
+    /// Moves the target of the turtle so it gives a "left-right" movement effect to the turtle.
+    /// </summary>
+    /// <returns></returns>
     IEnumerator WiggleTarget()
     {
         int direction = Random.Range(0, 3);
